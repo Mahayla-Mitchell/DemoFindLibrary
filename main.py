26966ba4ea3063a0da54bb23d5ba7a6ea1a630d1
Mahayla Mitchell <90640758+Mahayla-Mitchell@users.noreply.github.com>
2021-11-08 13:35:28 -0600

    Update main.py
    original

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from ctypes import cdll, c_double, byref
 import pathlib
-import os
 
 this_file_path = pathlib.Path(__file__)
 this_file_directory = this_file_path.parent.absolute()
-
 dll_path = this_file_directory / 'refprop.dll'
-ref_file_path = os.chdir('/Subdirect1')
+so_path = this_file_directory / 'libFakeRefProp.so'
+api = cdll.LoadLibrary(str(so_path))
 
-# so_path = ref_file_path / Subdirect1 / 'libFakeRefProp.so'
-# so_path = this_file_directory / Subdirect1 / 'libFakeRefProp.so'
-
-
-api = cdll.LoadLibrary(str(ref_file_path))
 arg_1 = c_double(1.0)
 arg_2 = c_double(2.0)
 arg_3 = c_double(0.0)

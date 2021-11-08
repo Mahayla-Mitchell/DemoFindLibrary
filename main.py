7fdbefaa2a468f1a0deb07705d3ca82eb8e3c1dd
Mahayla Mitchell <90640758+Mahayla-Mitchell@users.noreply.github.com>
2021-11-08 13:32:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ this_file_directory = this_file_path.parent.absolute()
 
 dll_path = this_file_directory / 'refprop.dll'
 ref_file_path = os.chdir('/Subdirect1')
-so_path = ref_file_path / Subdirect1 / 'libFakeRefProp.so'
 
+# so_path = ref_file_path / Subdirect1 / 'libFakeRefProp.so'
 # so_path = this_file_directory / Subdirect1 / 'libFakeRefProp.so'
 
 
-api = cdll.LoadLibrary(str(so_path))
+api = cdll.LoadLibrary(str(ref_file_path))
 arg_1 = c_double(1.0)
 arg_2 = c_double(2.0)
 arg_3 = c_double(0.0)

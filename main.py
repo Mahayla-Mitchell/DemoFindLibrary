8d5ba6aa89379a5253e920956f418dae73af4d7b
Mahayla Mitchell <90640758+Mahayla-Mitchell@users.noreply.github.com>
2021-11-09 13:48:43 -0600

    Update main.py
    trying something else

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pathlib
 
 this_file_path = pathlib.Path(__file__)
 this_file_directory = this_file_path.parent.absolute()
-this_subdir = pathlib.Path(this_file_directory).joinpath('Subdirect1')
+this_subdir = this file directort / Subdirect1 / 'libFakeRefProp.so'
 dll_path = this_file_directory / 'refprop.dll'
 so_path = this_file_directory / 'libFakeRefProp.so'
-api = cdll.LoadLibrary(str(so_path))
+api = cdll.LoadLibrary(str(this_subdir))
 
 arg_1 = c_double(1.0)
 arg_2 = c_double(2.0)
